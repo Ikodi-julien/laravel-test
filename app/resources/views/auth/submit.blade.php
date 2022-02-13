84aeabddc4138984739786726204f982513cfdb4
ikodi-julien <jupellin39@gmail.com>
2022-02-13 18:26:35 +0100

    FEAT: Add scss support and style sheet

--- app/resources/views/auth/submit.blade.php
+++ app/resources/views/auth/submit.blade.php
@@ -1,24 +1,38 @@
 @extends('layouts.app')
 @section('content')
+<section class="create_profil">
+    <h1>Formulaire de création d'un compte</h1>
     <form action="/submit" method="post">
         {{ csrf_field() }}
-        <input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value="{{ old('email')}}"
-        >
-        @if ($errors->has('email'))
-            <p>Le champ "email" a une erreur : {{ $errors->first('email') }}</p>
-        @endif
-        <input type="password" name="password" placeholder="Mot de passe">
-        @if ($errors->has('password'))
-            <p>Le champ "password" a une erreur : {{ $errors->first('password') }}</p>
-        @endif
-        <input type="password" name="password_confirmation" placeholder="Confirmer mdp">
-        @if ($errors->has('password_confirmation'))
-            <p>Le champ "password_confirmation" a une erreur : {{ $errors->first('password_confirmation') }}</p>
-        @endif
+        <div class="row">
+            <label for="email">Email :</label>
+            <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                value="{{ old('email')}}"
+            >
+            @if ($errors->has('email'))
+                <p>Le champ "email" a une erreur : {{ $errors->first('email') }}</p>
+            @endif
+        </div>
+        <div  class="row">
+
+            <label for="password">Mot de passe :</label>
+            <input type="password" name="password" placeholder="Mot de passe">
+            @if ($errors->has('password'))
+                <p>Le champ "password" a une erreur : {{ $errors->first('password') }}</p>
+            @endif
+        </div>
+        <div class="row">
+            <label for="password_confirmation">Confirmation du mot de passe :</label>
+            <input type="password" name="password_confirmation" placeholder="Confirmer mdp">
+            @if ($errors->has('password_confirmation'))
+                <p>Le champ "password_confirmation" a une erreur : {{ $errors->first('password_confirmation') }}</p>
+            @endif
+        </div>
+
         <input type="submit" value="M'inscrire">
     </form>
+</section>
 @endsection
