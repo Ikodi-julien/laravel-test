390e6b81c6c3fbf4fd9b7fd9f94c8022618ebf45
ikodi-julien <jupellin39@gmail.com>
2022-02-19 20:14:32 +0100

    MINOR: css on nav

--- app/resources/views/layouts/header.blade.php
+++ app/resources/views/layouts/header.blade.php
@@ -1,10 +1,32 @@
 <header>
     <nav>
-        <ul>
-            <li><a href="/">Accueil</a></li>
-            <li><a href="/signin">Créer un compte</a></li>
-            <li><a href="/connexion">Connexion</a></li>
-            <li><a href="/mon-compte">Mon compte</a></li>
-        </ul>
+        <div class="nav__container">
+            <a href="/"><span class="link {{request()->is('/') ? 'is-active' : ''}}">Accueil</span></a>
+            <div class="links">
+                @if (auth()->check())
+                <a href="/mon-compte ">
+                    <span class="link {{request()->is('mon-compte') ? 'is-active' : ''}}">
+                        Mon compte
+                    </span>
+                </a>
+                <a href="/logout">
+                    <span class="link {{request()->is('logout') ? 'is-active' : ''}}">
+                        Deconnexion
+                    </span>
+                </a>
+                @else
+                <a href="/signin">
+                    <span class="link {{request()->is('signin') ? 'is-active' : ''}}">
+                        Créer un compte
+                    </span>
+                </a>
+                <a href="/connexion">
+                    <span class="link {{request()->is('connexion') ? 'is-active' : ''}}">
+                        Connexion
+                    </span>
+                </a>
+                @endif
+            </div>
+        </div>
     </nav>
 </header>
